5c7cf5ff42a3a4cd48293838e0a2cb9b58e97952
Jiyong Youn <01@0101010101.com>
2025-06-27 01:23:50 +0900

    Init a web based project

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import globals from "globals";
+
+export default [
+  {
+    files: ['**/*.ts'],
+    ignores: [
+      'dist/**',
+      'node_modules/**',
+      'build/**'
+    ],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      },
+      globals: {
+        ...globals.node,
+        ...globals.browser
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-console': 'off'
+    }
+  }
+]
